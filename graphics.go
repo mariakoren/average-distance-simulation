81f061f11300d1819e391437ca6d447b07be0fd2
mariakoren <maria.korenn@gmail.com>
2024-06-02 14:56:32 +0200

    graphics

---
+++ graphics.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+)
+
+func main() {
+	filePath := "results.txt" 
+	dataX, dataY := readDataFromFile(filePath)
+
+	points := make(plotter.XYs, len(dataX))
+	for i := range dataX {
+		points[i].X = dataX[i]
+		points[i].Y = dataY[i]
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "Wykres z symulacji"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+	p.Add(plotter.NewGrid())
+
+	s, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	p.Add(s)
+
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "wykres.png"); err != nil {
+		panic(err)
+	}
+	fmt.Println("Wykres został zapisany do pliku wykres.png")
+}
+
+
+func readDataFromFile(filePath string) ([]float64, []float64) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var dataX, dataY []float64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		if len(fields) != 2 {
+			continue
+		}
+		x, err := strconv.ParseFloat(fields[0], 64)
+		if err != nil {
+			continue
+		}
+		y, err := strconv.ParseFloat(fields[1], 64)
+		if err != nil {
+			continue
+		}
+		dataX = append(dataX, x)
+		dataY = append(dataY, y)
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return dataX, dataY
+}
